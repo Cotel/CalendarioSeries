59d691ae138cdd22a8f77fbb4b032be309a1bce0
Cotel <miguelcoletomunoz@hotmail.com>
2016-02-11 21:21:11 +0100

    Empezado el modelo de Serie

---
+++ src/calendarioSeries/modelos/Serie.java
@@ -0,0 +1,122 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calendarioSeries.modelos;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.Arrays;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+
+/**
+ *
+ * @author Cotel
+ */
+public class Serie {
+    
+    private static final String BASE = "https://www.omdbapi.com/?";
+    
+    private String titulo;
+    private String[][] capitulos;
+    private int temporadas;
+    private JSONObject json;
+    
+    public Serie(String titulo) {
+        String toJson = readUrl(BASE + "t=" + titulo.toLowerCase() +
+                    "&type=series" + "&plot=short" + "&r=json");
+        this.json = new JSONObject(toJson);
+        
+        this.titulo = json.getString("Title");
+        
+        boolean next = true;
+        int temporada = 1;
+        int caps = 0;
+        JSONObject aux;
+        while(next) {
+            aux = new JSONObject(readUrl(BASE + "t=" + this.titulo + 
+                    "&Season=" + temporada + "&r=json"));
+            if(!aux.getString("Response").equals("False")) {
+                temporada++;
+                int actual = aux.getJSONArray("Episodes").length();
+                if(actual > caps) caps = actual;
+            } else {
+                next = false;
+            }
+        }
+        
+        this.temporadas = temporada;
+        this.capitulos = new String[temporadas][caps];
+        for(int i=0; i<capitulos.length; i++) {
+            aux = new JSONObject(readUrl(BASE + "t=" + this.titulo + 
+                    "&Season=" + i + "&r=json"));
+            JSONArray listaCaps = aux.getJSONArray("Episodes");
+            for(int j=0; j<capitulos[i].length; j++) {
+                capitulos[i][j] = i+1 + "x" + j+1 + " " + listaCaps.getJSONObject(j).getString("Title");
+            }
+        }
+        
+    }
+    
+    private String readUrl(String stringUrl) {
+        BufferedReader reader = null;
+        try {
+               URL url = new URL(stringUrl);         
+            try {
+                reader = new BufferedReader(new InputStreamReader(url.openStream()));
+                StringBuffer buffer = new StringBuffer();
+                int read;
+                char[] chars = new char[1024];
+                while((read = reader.read(chars)) != -1)
+                    buffer.append(chars, 0, read);
+                return buffer.toString();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if(reader != null) {
+                    reader.close();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String[][] getCapitulos() {
+        return capitulos;
+    }
+
+    public void setCapitulos(String[][] capitulos) {
+        this.capitulos = capitulos;
+    }
+
+    public int getTemporadas() {
+        return temporadas;
+    }
+
+    public void setTemporadas(int temporadas) {
+        this.temporadas = temporadas;
+    }
+    
+    
+    
+    public static void main(String[] args) {
+        Serie arrow = new Serie("Arrow");
+        System.out.println(Arrays.toString(arrow.getCapitulos()));
+    }
+    
+}
